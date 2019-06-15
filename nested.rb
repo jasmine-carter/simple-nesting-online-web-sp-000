ebb0f1fec6f6e3fddaf665836eda322993c95504
Jasmine Carter <jasmine.carter87@gmail.com>
2019-06-15 00:21:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 return programmer_hash[:dennis_ritchie][:languages](str)
+		 programmer_hash.fetch(:dennis_ritchie[:languages])
 end
 
 def adding_matz
